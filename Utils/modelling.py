15a1dc14caf31777d673e1d00911829c7f8f9366
stebil87 <stebill87@gmail.com>
2024-05-30 06:11:45 +0200

    results

--- Utils/modelling.py
+++ Utils/modelling.py
@@ -1,24 +1,16 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import LeaveOneOut
 from sklearn.metrics import mean_absolute_error
 from xgboost import XGBRegressor
 from lightgbm import LGBMRegressor
 from sklearn.ensemble import AdaBoostRegressor
 import warnings
+
+# Corrected warning filter
 warnings.filterwarnings("ignore", category=UserWarning, module='lightgbm')
 
-def create_hankel_matrix(X, n_rows=None):
-    hankel_data = []
-    for column in X.columns:
-        data = X[column].values
-        if n_rows is None:
-            n_rows = len(data) // 2
-        hankel_matrix = np.array([data[i: i + n_rows] for i in range(len(data) - n_rows + 1)])
-        hankel_data.append(hankel_matrix.flatten())
-    return pd.DataFrame(hankel_data).transpose()
-
-def perform_regression_and_cv(dictionaries, use_hankel=False):
+def perform_regression_and_cv(dictionaries):
     results = {}
     loo = LeaveOneOut()
 
@@ -35,10 +27,6 @@ def perform_regression_and_cv(dictionaries, use_hankel=False):
             y_train = pd.concat([df['y'] for df in train_dfs])
             X_test = test_df.drop(columns='y', errors='ignore')
             y_test = test_df['y']
-
-            if use_hankel:
-                X_train = create_hankel_matrix(X_train)
-                X_test = create_hankel_matrix(X_test)
 
             models = {
                 'XGBoost': XGBRegressor(),
@@ -63,5 +51,3 @@ def print_results(results, description):
         for model_name, maes in dict_results.items():
             avg_mae = np.mean(maes)
             print(f"  Model: {model_name}, MAE: {avg_mae:.4f}")
-
-
